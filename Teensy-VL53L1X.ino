e79743cab606ff7caed59a771e424e2b9f2c419e
Arnaud <nanaud7@outlook.fr>
2022-04-11 22:37:42 +0200

    2 sensors

--- Teensy-VL53L1X.ino
+++ Teensy-VL53L1X.ino
@@ -4,24 +4,37 @@
 #define VL53L1X_GPIO1 24
 #define VL53L1X_XSHUT1 25
 
+#define VL53L1X_GPIO2 26
+#define VL53L1X_XSHUT2 27
+
+#define VL53L1X_GPIO3 28
+#define VL53L1X_XSHUT3 29
+
 /* Variables -----------------------------------------------------*/
-VL53L1X_ULD sensor;
-bool dataReady;
+VL53L1X_ULD sensor2;
+VL53L1X_ULD sensor3;
+bool dataReady2;
+bool dataReady3;
 
 void setup() {
   Serial.begin(115200); // Start the serial port
   Wire.begin(); // Initialize the I2C controller
 
-  // XSHUT pin initialization
-  pinMode(VL53L1X_XSHUT1, OUTPUT);
-  digitalWrite(VL53L1X_XSHUT1, HIGH);
+  // Sensor n°2 pin init
+  pinMode(VL53L1X_GPIO2, INPUT);
+  pinMode(VL53L1X_XSHUT2, OUTPUT);
+  digitalWrite(VL53L1X_XSHUT2, LOW);
+  
+  // Sensor n°3 pin init
+  pinMode(VL53L1X_GPIO3, INPUT);
+  pinMode(VL53L1X_XSHUT3, OUTPUT);
+  digitalWrite(VL53L1X_XSHUT3, LOW);
 
-  // Set the pin to INPUT as it will be used as an interrupt
-  // Technically not nedded as all pins will be defined as inputs on reset
-  pinMode(VL53L1X_GPIO1, INPUT);
-
+  delay(500);
+  digitalWrite(VL53L1X_XSHUT2, HIGH);
+ 
   // Initialize the sensor
-  VL53L1_Error status = sensor.Begin();
+  VL53L1_Error status = sensor2.Begin();
   if (status != VL53L1_ERROR_NONE) {
     // If the sensor could not be initialized print out the error code. -7 is timeout
     Serial.println("Could not initialize the sensor, error code: " + String(status));
@@ -29,38 +42,72 @@ void setup() {
   }
   Serial.println("Sensor initialized");
 
-  // It is also possible to change the interrupt polarity of the sensor.
-  // By default it is ActiveHIGH which means the pin will go HIGH when a measurement is done
-  
-  // If you want to reverse the polarity to ActiveLOW uncomment the following line and change the attachInterrupt from RISING to FALLING
-  //sensor.SetInterruptPolarity(ActiveLOW); // Possible values are ActiveHIGH and ActiveLOW
-  
   // Read out the interrupt polarity
-  EInterruptPolarity polarity; // 0 = ActiveLow, 1 = ActiveHigh
-  sensor.GetInterruptPolarity(&polarity);
-  Serial.println("Interrupt polarity: " + String((uint8_t)polarity));
+  EInterruptPolarity polarity2; // 0 = ActiveLow, 1 = ActiveHigh
+  sensor2.GetInterruptPolarity(&polarity2);
+  Serial.println("Interrupt polarity: " + String((uint8_t)polarity2));
 
-  attachInterrupt(digitalPinToInterrupt(VL53L1X_GPIO1), ReadData, RISING);
+  attachInterrupt(digitalPinToInterrupt(VL53L1X_GPIO2), ReadData2, RISING);
+
+  sensor2.SetI2CAddress(0x60);
+
+  delay(500);
+  digitalWrite(VL53L1X_XSHUT3, HIGH);
+ 
+  // Initialize the sensor
+  status = sensor3.Begin();
+  if (status != VL53L1_ERROR_NONE) {
+    // If the sensor could not be initialized print out the error code. -7 is timeout
+    Serial.println("Could not initialize the sensor, error code: " + String(status));
+    while (1) {}
+  }
+  Serial.println("Sensor initialized");
+
+  // Read out the interrupt polarity
+  EInterruptPolarity polarity3; // 0 = ActiveLow, 1 = ActiveHigh
+  sensor3.GetInterruptPolarity(&polarity3);
+  Serial.println("Interrupt polarity: " + String((uint8_t)polarity3));
+
+  attachInterrupt(digitalPinToInterrupt(VL53L1X_GPIO3), ReadData3, RISING);
 
   // Start ranging
-  sensor.StartRanging();
+  sensor2.StartRanging();
+  sensor3.StartRanging();
 }
 
 void loop() {
-  if (dataReady) {
+  if (dataReady2) {
     // Get the results
-    uint16_t distance;
-    sensor.GetDistanceInMm(&distance);
+    uint16_t distance2;
+    sensor2.GetDistanceInMm(&distance2);
 
     // After reading the results reset the interrupt to be able to take another measurement
-    sensor.ClearInterrupt();
-    dataReady = false;
+    sensor2.ClearInterrupt();
+    dataReady2 = false;
 
-    Serial.println("Distance in mm: " + String(distance));
+    Serial.println("Distance2 in mm: " + String(distance2));
+    delay(500);
+  }
+
+  if (dataReady3) {
+    // Get the results
+    uint16_t distance3;
+    sensor3.GetDistanceInMm(&distance3);
+
+    // After reading the results reset the interrupt to be able to take another measurement
+    sensor3.ClearInterrupt();
+    dataReady3 = false;
+
+    Serial.println("Distance3 in mm: " + String(distance3));
+    delay(500);
   }
 }
 
 // This function runs everytime the sensor has data available
-void ReadData() {
-  dataReady = true;
+void ReadData2() {
+  dataReady2 = true;
+}
+
+void ReadData3() {
+  dataReady3 = true;
 }
